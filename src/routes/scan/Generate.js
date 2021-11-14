1575fbaf678d1107630313807221ba7ec50796d2
CalinDS <calinds2009@yahoo.com>
2021-11-14 14:53:08 +0200

    QR generator and scanner updated

--- src/routes/scan/Generate.js
+++ src/routes/scan/Generate.js
@@ -1,16 +1,57 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import QRCode from "react-qr-code";
-import './Scan.scss';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+import './Generate.scss';
+
+// GET id_activitate_curenta, parametru = id_profesor
 
 export default function Generate() {
 
+    const [dateTime, setDateTime] = useState(new Date().toISOString());
+    const [isOpened, setIsOpened] = useState(true);
+
+    // id_prof: localStorage.getItem('id_user');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDateTime(new Date().toISOString());
+        }, 10000);
+        return () => clearInterval(interval);
+        }, []);
+    
+
+    let qrValue =  "ID," + dateTime;
+
+    const handleGenerate = data => {
+        setIsOpened(true);
+        setDateTime(new Date().toISOString());
+    }
+
+    const handleHide = () => {
+        setIsOpened(false);
+    }
 
     return (
         <div>
-        
-            <QRCode className="qr-code" value="Materie Profesor Ora" />
             
+            <div className={!isOpened ? "invisible" : ""}>
+                <p>{qrValue}</p>
+                <QRCode className="qr-code" value={qrValue}  />
+            </div>
+
+            <div className="flex-container"> 
+                <div className="flex-element">
+                    <Button className="button" variant="outlined" onClick={handleHide}>Hide QR</Button>
+                </div>
+                <div className="flex-element">
+                    <Button className="button" variant="contained" onClick={handleGenerate}>Generate QR</Button>
+                </div>
+            </div>
+           
+
         </div>
-      );
+    );
 
 }
--- src/routes/scan/Scan.js
+++ src/routes/scan/Scan.js
@@ -1,24 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import QrReader from 'react-qr-reader';
 import './Scan.scss';
 
+async function sendQRCode(payload) {  // id_activit, ora generare qr, 
+    return fetch('http://127.0.0.1:5000/stats', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(data => {
+        if (!data.ok)  {
+          return;
+        }
+        return data.json();
+      }); 
+   }
+  
+
 export default function Scan() {
 
-  const [scan, setScan] = useState("no result");
- 
-  const handleScan = data => {
-    if (data) {
-      setScan(data);
-    }
-  }
+    const [scan, setScan] = useState("no result");
 
-  return (
-    <div>
-        <h2>TODO: Dragos</h2>
+    const [oldScan, setOldScan] = useState("no result");
 
-        <QrReader className="qr-code" onScan={handleScan} />
-        <p>{scan}</p>
+    const handleScan = async data => {
         
-    </div>
-  );
+        if (data) {
+            setScan(data);
+            if (data !== oldScan) {
+                console.log("data: " + data + " old " + oldScan);
+                setOldScan(data);
+                const beans = data.split(',');
+                const err = await sendQRCode({
+                    activityId: beans[0],
+                    dateTime: beans[1],
+                    studentId: "1mock1"
+                    // id_student: localStorage.getItem('id_user');
+                });
+            }
+        }
+        
+    }
+
+    
+    
+
+    return (
+        <div>
+            <h2 className="info">Nume Materie <br /> Nume Profesor </h2>
+            <QrReader className="qr-code" onScan={handleScan}/>
+            <p className="info">{scan}</p>
+        </div>
+    );
 }
